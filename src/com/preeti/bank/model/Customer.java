45d6d3d380dee3ccb7ab28efcbc12c5fd74f13ff
Preeti <jha.preeti0307@gmail.com>
2020-07-22 12:31:04 +1000

    First Checkin

---
+++ src/com/preeti/bank/model/Customer.java
@@ -0,0 +1,13 @@
+package com.preeti.bank.model;
+
+public class Customer {
+	int custId;
+	String custName;
+	String addresss;
+	
+	public Customer(int custId, String custName){
+		this.custId = custId;
+		this.custName = custName;
+	}
+
+}
